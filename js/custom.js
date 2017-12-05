19fea64aa6df20bd3d17841a69319a684c20d68d
Neeraj Kumar <sainineeraj1234@gmail.com>
2017-12-05 18:12:32 +0530

    Updated layout

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,9 @@
 var remote = require('electron').remote;
-var BrowserWindow = remote.BrowserWindow;
-var ipcMain = remote.ipcMain;
 var app = remote.app;
-var url = require('url');
 var path = require('path');
 var dblite = require('dblite').withSQLite('3.8.6+');
 var dbpath = path.join(app.getPath('userData'), 'db.sqlite3');
 var db = dblite(dbpath);
-var window = remote.getCurrentWindow();
 var handlebar = require('handlebars');
 var selectors = {
     title: $('[name="title"]'),
@@ -15,16 +11,17 @@ var selectors = {
     topic: $('[name="topic"]'),
     year: $('[name="year"]'),
     isbn: $('[name="isbn"]'),
-    sub_topic: $('[name="sub-topic"]'),
+    sub_topic: $('[name="sub-topic"]')
 };
-var arr = ['title', 'author', 'topic', 'year', 'isbn', 'sub_topic'];
+var entry_model = '#entry-model';
 var success_alert = $('#card-alert-success');
 var error_alert = $('#card-alert-error');
+
 function get_or_create_table() {
     var query = "SELECT name FROM sqlite_master WHERE type='table' AND name='book';";
     db.query(query, function (err, rows) {
         if (rows.length < 1) {
-            query = "CREATE TABLE book\n" +sqlite_master
+            query = "CREATE TABLE book\n" +
                 "(\n" +
                 "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
                 "    title VARCHAR NOT NULL,\n" +
@@ -42,110 +39,70 @@ function get_or_create_table() {
 }
 
 
-function openNewWindow(template_name, parameter) {
-    var param = "";
-    if (parameter)
-        param = "?" + parameter;
-    let win = new BrowserWindow({width: window.innerWidth, height: window.innerHeight});
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, '../templates/' + template_name + '.html'),
-        protocol: 'file:',
-        slashes: true
-    }) + param);
-    win.maximize();
-    window.close();
-}
-
-function get_param_value(param) {
-    var sPageURL = location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == param) {
-            return sParameterName[1];
-        }
-    }
-}
-
-function set_param() {
-    var param = "";
-    for (i = 0; i < arr.length; i++) {
-        var value = selectors[arr[i]].val();
-        if (value)
-            param += arr[i] + "=" + value + "&"
-    }
-    return param
-}
-
-function get_search_query() {
-    var query = "";
-    var value = get_param_value('search');
-    for (i = 0; i < arr.length; i++) {
-        if (value && query)
-            query += ' OR ' + arr[i] + ' LIKE "%' + value + '%" ';
-        else if (value)
-            query = arr[i] + ' LIKE "%' + value + '%" '
-    }
-    return query
-}
-
-$(document).on('click', '#view-all-btn', function () {
-    openNewWindow('list',undefined)
-});
-$(document).on('click', '#add-new_entry-btn', function () {
-    openNewWindow('add',undefined)
-});
-
-$(document).on('click', '#update-back-btn', function () {
-    openNewWindow('list', undefined)
-});
-
-$(document).on('click', '#list-back-btn', function () {
-    openNewWindow('index', undefined)
-});
-
-$(document).on('click', '#search-back-btn', function () {
-    openNewWindow('index', undefined)
-});
-$(document).on('click', '#search-entry', function () {
-    // var param = set_param();
-    var param =  $('[name="search"]').val();
-    openNewWindow('list', "search=" + param)
-});
 $(document).on('click', '.update-data', function () {
     var id = $(this).data('id');
-    openNewWindow('update', "book=" + id);
+    db.query('SELECT * from book where id=' + id, function (err, rows) {
+        selectors.title.val(rows[0][1]);
+        selectors.author.val(rows[0][2]);
+        selectors.topic.val(rows[0][3]);
+        selectors.year.val(rows[0][4]);
+        selectors.isbn.val(rows[0][5]);
+        selectors.sub_topic.val(rows[0][6]);
+        $(entry_model + ' .title').text('Update Book');
+        $(entry_model + ' .successful-msg').text('Entry Updated Successfully');
+        $(entry_model + ' [name="id"]').val(id);
+        $('#add-entry').text('Update Entry');
+        $(entry_model).modal('open');
+        M.updateTextFields();
+    });
 });
 
 $(document).on('click', '#add-entry', function () {
+    var id = $(entry_model + ' [name="id"]').val();
     var title = selectors.title.val();
     var author = selectors.author.val();
     var topic = selectors.topic.val();
     var year = selectors.year.val();
     var isbn = selectors.isbn.val();
     var sub_topic = selectors.sub_topic.val();
-    if (title && author && topic && year && isbn && sub_topic) {
+    if (id) {
+        if (title && author && topic && year && isbn && sub_topic) {
+            db.query('UPDATE book SET title="' + title + '", author="' + author + '", topic="' + topic +
+                '", year="' + year + '", isbn="' + isbn + '", sub_topic="' + sub_topic + '" where id=' + id + ';');
+            error_alert.hide();
+            success_alert.show();
+            location.reload();
+        }
+        else {
+            success_alert.hide();
+            error_alert.show()
+        }
+    }
+    else if (title && author && topic && year && isbn && sub_topic) {
         db.query('INSERT INTO book VALUES(null,?,?,?,?,?,?)', [title, author, topic, year, isbn, sub_topic]);
         error_alert.hide();
-        success_alert.show()
+        success_alert.show();
+        location.reload();
     }
     else {
         success_alert.hide();
         error_alert.show()
     }
 });
+
+function filterGlobal() {
+    $('#datatable').DataTable().search(
+        $('input#search').val()
+    ).draw();
+}
+
 $(document).ready(function () {
     if ($('#list-template').length) {
-        var param = get_search_query();
-        var search_query = "";
-        if (param)
-            search_query = 'WHERE (' + param + ');';
-        var query = 'select * from book ' + search_query;
-        var data = db.query('select * from book ' + search_query, function (err, rows) {
+        db.query('select * from book ORDER BY id DESC;', function (err, rows) {
             var source = document.getElementById("list-template").innerHTML;
             var template = handlebar.compile(source);
             var context = "";
-            for (i = 0; i < rows.length; i++) {
+            for (var i = 0; i < rows.length; i++) {
                 context += template({
                     id: rows[i][0], counter: i + 1,
                     title: rows[i][1], author: rows[i][2],
@@ -154,30 +111,33 @@ $(document).ready(function () {
                 });
             }
             $('tbody').html(context);
+            $('#datatable').DataTable({
+                "paging": false,
+                "ordering": false
+            });
+            $('input#search').on('keyup click', function () {
+                filterGlobal();
+            });
         });
     }
-    if ($('#update-page').length) {
-        var id = get_param_value('book');
-        db.query('SELECT * from book where id=' + id, function (err, rows) {
-            selectors.title.val(rows[0][1]);
-            selectors.author.val(rows[0][2]);
-            selectors.topic.val(rows[0][3]);
-            selectors.year.val(rows[0][4]);
-            selectors.isbn.val(rows[0][5]);
-            selectors.sub_topic.val(rows[0][6]);
-            M.updateTextFields();
-        })
-    }
-    $(".alert .close").click(function(){$(this).closest(".alert").fadeOut("slow")});
+    $(".alert .close").click(function () {
+        $(this).closest(".alert").fadeOut("slow")
+    });
     get_or_create_table();
     $('.modal').modal({
             dismissible: true,
             opacity: .5,
             startingTop: '20%',
             endingTop: '10%',
+            complete: function () {
+                $("#entry-form")[0].reset();
+                $(entry_model + ' .title').text('Add New Book');
+                $(entry_model + ' .successful-msg').text('New Entry added successfully');
+                $(entry_model + ' [name="id"]').val('');
+                $('#add-entry').text('Add Entry');
+            }
         }
     );
-
 });
 
 
@@ -191,24 +151,3 @@ $(document).on('click', '#delete-selected', function () {
     location.reload();
 
 });
-
-$(document).on('click', '#update-data-btn', function () {
-    var id = get_param_value('book');
-    var title = selectors.title.val();
-    var author = selectors.author.val();
-    var topic = selectors.topic.val();
-    var year = selectors.year.val();
-    var isbn = selectors.isbn.val();
-    var sub_topic = selectors.sub_topic.val();
-    if (title && author && topic && year && isbn && sub_topic) {
-        db.query('UPDATE book SET title="' + title + '", author="' + author + '", topic="' + topic +
-            '", year="' + year + '", isbn="' + isbn + '", sub_topic="' + sub_topic + '" where id=' + id + ';');
-        error_alert.hide();
-        success_alert.show()
-    }
-    else {
-        success_alert.hide();
-        error_alert.show()
-    }
-});
-
--- main.js
+++ main.js
@@ -6,14 +6,10 @@ const {app, BrowserWindow, ipcMain} = electron;
 const path = require('path');
 const url = require('url');
 
-const dblite = require('dblite');
-const dbpath = path.join(app.getPath('userData'), 'db.sqlite3');
-const db = dblite(dbpath);
-
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow();
